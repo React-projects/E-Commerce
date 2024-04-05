72ce4a3f75aeb1e99bdb971ba13b306962b38cc0
BAssal <baslm8496@gmail.com>
2024-04-06 01:35:39 +0200

    customizing projects files

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar"
+
+function HomePage() {
+  return (
+    <div>
+        <NavBar/>
+        Home page
+      
+    </div>
+  )
+}
+
+export default HomePage
