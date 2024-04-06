fc525af52728c6cbaadf8bd0481a42def15fa21d
BAssal <baslm8496@gmail.com>
2024-04-06 16:01:06 +0200

    editing some imports and css

--- src/components/Home/HomeCategory.jsx
+++ src/components/Home/HomeCategory.jsx
@@ -1,21 +1,22 @@
 import { Container, Row } from "react-bootstrap";
 import { images } from "../../assets/Imports/images";
 import CategoryCard from "../Category/CategoryCard";
-import SubTitle from "../Utility/SubTitl3";
+import SubTitle from "../Utility/SubTitle";
 
 const HomeCategory = () => {
   return (
     <Container>
       <SubTitle title=" التصنيفات" btntitle="المزيد"  />
       <Row className="my-2 ">
-        <CategoryCard img={images.clothe} background="#F4DBA5" title='feufbeuifb' />
-        <CategoryCard img={images.cat2} background="#0034FF"title='feufbeuifb'  />
-        <CategoryCard img={images.labtop} background="#FFD3E8"title='feufbeuifb'  />
-        <CategoryCard img={images.clothe} background="#55CFDF"title='feufbeuifb'  />
-        <CategoryCard img={images.sale} background="#FF6262"title='feufbeuifb'  />
-        <CategoryCard img={images.pic} background="#F4DBA5" title='feufbeuifb' />
+        <CategoryCard img={images.clothe} background="#F4DBA5" title='اجهزه منزليه' />
+        <CategoryCard img={images.cat2} background="#0034FF"title='ادوات مطبخ'  />
+        <CategoryCard img={images.labtop} background="#FFD3E8"title='تخفيضات'  />
+        <CategoryCard img={images.clothe} background="#55CFDF"title='ملابس رجال'  />
+        <CategoryCard img={images.sale} background="#FF6262"title='لاتوبس'  />
+        <CategoryCard img={images.pic} background="#F4DBA5" title=' ملابس اطفال ' />
       </Row>
     </Container>
+    
   );
 };
 
